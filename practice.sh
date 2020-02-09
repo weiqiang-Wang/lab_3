9d43d9be4069199aca5b91ee32ed136ca758597e
weiqiang-Wang <wewa2282@colorado.edu>
2020-02-08 21:49:30 -0700

    Adding all of the files for lab 3

--- practice.sh
+++ practice.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# Author : Your Name
-# Date: Today's date
-# Script follows here:
-echo "Enter a number: "
-read numOne
-echo "Enter a second number: "
-read numTwo
-sum=$(($numOne + $numTwo))
-echo "The sum is : $sum"
-let prod=numOne*numTwo
-echo "The product is: $prod"
-echo "File Name: $0"
-echo "Command Line Argument 1: $1"
-grep $1 $2
+# Authors : Weiqiang Wang & Garrett Hite
+# Date: 1/1/2019
+
+#Problem 1 Code
+
+#Make sure to document how you are solving each problem!
+echo "Input yout filename"
+read filename
+echo "Input regEx"
+read regEx
+egrep $regEx $filename
+echo " the number of phone numbers:"
+egrep -c ^[0-9]{3}-[0-9]{3}-[0-9]{4} $filename
+echo "count of the number of emails"
+egrep -c @ $filename
+echo " all the phone numbers in the "303" area code"
+egrep -o ^303-[0-9]{3}-[0-9]{4} $filename
+grep geocities.com $filename >> email_result.txt
